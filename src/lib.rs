2a16088428f3ec22196aad4cfc8f91af8d212942
nakamurusun <3872mn@gmail.com>
2020-03-31 18:15:33 +0900

    modified using iterator

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,23 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match.args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name")
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
+
         // cloneメソッドで、Configインスタンスが所有するデータの総コピーが生成される
         // 文字列データへの参照を保持するより時間とメモリを消費するが、
         // 参照のライフライム管理が不要に
@@ -52,32 +68,17 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        // line() => 文字列を行ごとに繰り返すメソッド
-        if line.contains(query) {
-            // contains() => 行がquery文字列を含むか確認
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a
-    str> {
-        let query = query.to_lowercase();
-        let mut results = Vec::new();
-
-        for line in contents.lines() {
-            if line.to_lowercase().contains(&query) {
-                results.push(line);
-            }
-        }
-
-        results
-    }
+     str> {
+         let query = query.to_lowercase();
+         contents.lines()
+             .filter(|line| line.to_lowercase().contains(&query))
+     }
 
 #[cfg(test)]
 mod test {
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ use std::process;
 use minigrep::Config; // Config 型をスコープに持ってくる
 
 fn main() {
- 	// collect関数を使い、文字列のベクタがほしいと明示的に注釈
-    let args: Vec<String> = env::args().collect();
 
     // unwrap_or_elseは標準ライブラリで、独自のエラー処理を定義
     // Ok値ならOkが包む中身の値を返す
     // Err値なら、クロージャ内でコードを呼び出す
     // 静的文字列のErrの中身を、縦棒の間のerr引数のクロージャに渡している
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
